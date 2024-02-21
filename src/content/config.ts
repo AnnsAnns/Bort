75ec49e06cd387bf7a1140d50f50f517d0a55c09
AnnsAnn <git@annsann.eu>
2024-02-21 13:28:53 +0100

    Port every single blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,7 +11,7 @@ const blog = defineCollection({
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 		tags: z.array(z.string()).optional(),
-		isLegacy: z.boolean().optional(),
+		legacy: z.boolean().optional(),
 		sideVideo: z.string().optional(),
 	}),
 });
