d09caa7e13a4555a36cb5d95d06c7422a128e05d
AnnsAnn <git@annsann.eu>
2024-02-22 17:52:19 +0100

    Fix prefetch issue (Is already prefetching)

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://annsann.eu',
-  prefetch: true,
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
