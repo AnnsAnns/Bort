a035f057ccffdbc7c7dd6eba659d00c8dfd9515d
AnnsAnn <git@annsann.eu>
2024-03-01 11:41:55 +0100

    Make the site a PWA because why not

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://annsann.eu',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), serviceWorker()]
 });
