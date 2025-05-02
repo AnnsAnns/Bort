efd6e1fc266f5b588456f6e79299db40f1385fc4
AnnsAnn <git@annsann.eu>
2025-05-02 12:12:56 +0200

    change base name in previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import embeds from 'astro-embed/integration';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+// Github Previews have a different base URL than production
+const base_url = process.env.PREVIEW_PATH ? process.env.PREVIEW_PATH : '/';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://annsann.eu',
-  integrations: [embeds(), mdx(), sitemap(), tailwind()]
+  integrations: [embeds(), mdx(), sitemap(), tailwind()],
+  base: base_url,
 });
