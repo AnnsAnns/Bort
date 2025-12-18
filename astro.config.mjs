8c7d637f9a92bd90ad9ca625e3ba9ac374396220
AnnsAnn <git@annsann.eu>
2025-12-18 15:29:59 +0100

    Youtube embed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import embeds from 'astro-embed/integration';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // Github Previews have a different base URL than production
-const base_url = process.env.PREVIEW_PATH ? process.env.PREVIEW_PATH : '/';
+const base_url = process.env.PREVIEW_PATH ? process.env.PREVIEW_PATH : "/";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://annsann.eu',
-  integrations: [embeds(), mdx(), sitemap()],
+  site: "https://annsann.eu",
+  integrations: [mdx(), sitemap()],
   base: base_url,
   prefetch: {
     prefetchAll: true,
