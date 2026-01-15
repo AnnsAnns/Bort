0fe24e28b3528f0248829719d6e2731478813bec
AnnsAnns <git@annsann.eu>
2026-01-15 12:30:04 +0000

    Deploying to pages from @ AnnsAnns/Bort@955671b815cf17616fe7bebb7e0e6e0ebadbd769 🚀

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', function(_e) {
+    self.skipWaiting();
+  });
+  
+self.addEventListener('activate', function(_e) {
+    self.registration.unregister()
+        .then(function() {
+        return self.clients.matchAll();
+        })
+        .then(function(clients) {
+        clients.forEach(client => client.navigate(client.url))
+        });
+});
