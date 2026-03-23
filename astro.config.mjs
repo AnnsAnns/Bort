96179c168b10d58ffe648c07a3ce196ee93df50e
AnnsAnn <git@annsann.eu>
2026-03-23 14:01:57 +0100

    Add aly blinky

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,19 @@ const base_url = process.env.PREVIEW_PATH ? process.env.PREVIEW_PATH : "/";
 // https://astro.build/config
 export default defineConfig({
   site: "https://annsann.eu",
+  output: "static",
   integrations: [mdx(), sitemap()],
   base: base_url,
   prefetch: {
     prefetchAll: true,
+  },
+  experimental: {
+    svgo: true,
+  },
+  image: {
+    responsiveStyles: true,
+    layout: "constrained",
+    remotePatterns: [{ protocol: "https" }],
   },
   vite: {
     plugins: [tailwindcss()],
